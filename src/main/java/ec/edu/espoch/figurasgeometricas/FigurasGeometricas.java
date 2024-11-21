e627c837f3f9688406890443c628d14556ef6dd5
Zanyn-user <zanynuser@gmail.com>
2024-11-21 13:49:45 -0500

    Creación del proyecto

---
+++ src/main/java/ec/edu/espoch/figurasgeometricas/FigurasGeometricas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.figurasgeometricas;
+
+/**
+ *
+ * @author User
+ */
+public class FigurasGeometricas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
